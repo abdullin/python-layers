e1815e67c2ac12333c26a79f116d8f90256cd241
Jed Estep <austin.estep@10gen.com>
2014-07-17 10:39:03 -0400

    more pythonic packaging

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+requires = [
+    'foundationdb'
+]
+
+setup(
+    name='fdb_layers',
+    version='1.0.0',
+    packages='fdb_layers',
+    description='FoundationDB layers for Python'
+)
